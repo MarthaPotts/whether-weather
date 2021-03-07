665ee4e293374d65fb3a7a29f895b1f530eba647
Martha Potts <marthajpotts7@gmail.com>
2021-03-07 13:08:12 -0500

    small start to functions

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,29 @@
 //assignment
 
 
-
+//get the html linked to js
 //functions
+//use getApi function from class:
+
+function getApi(){
+  //set url to variable
+  var requestUrl = '#'
+  //pass url through fetch()
+  fetch(requestUrl)
+  //"makes a promise" to return the response from the request sent to the url
+    .then(function(response){
+      // return response passed through .json(), string/object data
+       return response.json();
+    })
+    .then(function(data){ //promise to return data
+      console.log(data) //console.log the data
+    })
+    //loop over the data to sort/render to the window
+    for(var i=-0; i < data.length; i++){
+      //create the elements needed/wanted to render
+      
+    }
+}
   // retrieve city weather data from API and put in local strorage
 
   //sort local storage and render in specific sections/elements
@@ -10,3 +31,4 @@
 
 //eventlisteners
   //search-btn click needs to trigger function to fetch data, add a city, clear
+  //saved cities by click
